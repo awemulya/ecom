31d3a561083613a807a0efeb2b4325d8a2ad03cd
awemulya <awemulya@gmail.com>
2016-06-13 17:04:01 +0545

    models inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField()
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=256)
+    rate = models.FloatField(null=True)
+    company = models.ForeignKey(Company)
+
+class OtherProperties(models.Model):
+    item = models.ForeignKey(Item, related_name='properties')
+    name = models.CharField(max_length=256)
+    value = models.CharField(max_length=256)
+
+class ItemImages(models.Model):
+    item = models.ForeignKey(Item, related_name='images')
+    file = models.ImageField()
